5a6c3cd2df002a2026638a3df02ccfeed775f52b
Noah <buchenn@oregonstate.edu>
2018-06-02 09:30:46 -0400

    1st pull request, main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+/***************************************************************************
+** Name: TSP Project
+** Authors: Noah Buchen, On Pong Brian Tsui, Emily Dierks 
+** Date: 2 June 2018
+** Description: This program will provide an approximate solution for the 
+** classic TSP problem. It will accept a command line data file input and 
+** output a text file with the solution data called "inputfile".tour
+***************************************************************************/
